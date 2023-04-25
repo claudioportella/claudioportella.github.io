327893d623e2f03ef317d273fdba69372e487622
Claudio Portella <claudio@claudioportella.com>
2023-04-25 19:58:47 -0300

    Add files via upload

--- lombada/script.js
+++ lombada/script.js
@@ -1,13 +1,13 @@
 const papeis = [
-  { tipo: "offset 63g", semCostura: 0.042, comCostura: 0.044 },
-  { tipo: "offset 75g", semCostura: 0.050, comCostura: 0.052 },
-  { tipo: "offset 90g", semCostura: 0.058, comCostura: 0.062 },
-  { tipo: "couchê 90g", semCostura: 0.044, comCostura: 0.046 },
-  { tipo: "couchê 120g", semCostura: 0.060, comCostura: 0.062 },
-  { tipo: "couchê 150g", semCostura: 0.070, comCostura: 0.072 },
-  { tipo: "pólen soft 70g", semCostura: 0.048, comCostura: 0.050 },
-  { tipo: "pólen soft 80g", semCostura: 0.054, comCostura: 0.057 },
-  { tipo: "vergê 85g", semCostura: 0.059, comCostura: 0.061 },
+  { tipo: "offset 75g", colada: 0.050, costurada: 0.052 },
+  { tipo: "offset 63g", colada: 0.042, costurada: 0.044 },
+  { tipo: "offset 90g", colada: 0.058, costurada: 0.062 },
+  { tipo: "couchê 90g", colada: 0.044, costurada: 0.046 },
+  { tipo: "couchê 120g", colada: 0.060, costurada: 0.062 },
+  { tipo: "couchê 150g", colada: 0.070, costurada: 0.072 },
+  { tipo: "pólen soft 70g", colada: 0.048, costurada: 0.050 },
+  { tipo: "pólen soft 80g", colada: 0.054, costurada: 0.057 },
+  { tipo: "vergê 85g", colada: 0.059, costurada: 0.061 },
 ];
 
 function gerarCamposPapel() {
@@ -26,7 +26,7 @@ function calcularLombada() {
   const encadernacao = document.getElementById("encadernacao").value;
   const paginas = document.getElementById("numPaginas").value;
   const papel = papeis[papelIndex];
-  const lombada = encadernacao === "semCostura" ? papel.semCostura : papel.comCostura;
+  const lombada = encadernacao === "colada" ? papel.colada : papel.costurada;
   const resultado = paginas * lombada;
 
   document.getElementById("resultado").innerHTML = resultado.toLocaleString('pt-BR', {
@@ -35,8 +35,8 @@ function calcularLombada() {
   }) + " mm";
 
    const obs = document.getElementById("obs");
-  if (resultado < 4) {
-    obs.innerHTML = "Para lombadas com menos de 4mm o recomendável é grampo canoa";
+  if (resultado < 3) {
+    obs.innerHTML = "⚠️<br>Espessura com menos de 3 mm.<br> Encadernar com grampo canoa.";
   } else {
     obs.innerHTML = "";
   }
