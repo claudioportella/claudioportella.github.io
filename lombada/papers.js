b588ba5b71d524f9193e6bb35924d40e9f8d7f76
Claudio Portella <claudio@claudioportella.com>
2026-03-17 19:00:39 -0300

    split into 4 files

---
+++ lombada/papers.js
@@ -0,0 +1,12 @@
+const papeis = [
+    { tipo: "Offset 75g", colada: 0.050, costurada: 0.052 },
+    { tipo: "Offset 63g", colada: 0.042, costurada: 0.044 },
+    { tipo: "Offset 90g", colada: 0.058, costurada: 0.062 },
+    { tipo: "Offset 120g", colada: 0.065, costurada: 0.067 },
+    { tipo: "Couchê 90g", colada: 0.044, costurada: 0.046 },
+    { tipo: "Couchê 120g", colada: 0.060, costurada: 0.062 },
+    { tipo: "Couchê 150g", colada: 0.070, costurada: 0.072 },
+    { tipo: "Pólen Soft 70g", colada: 0.048, costurada: 0.050 },
+    { tipo: "Pólen Soft 80g", colada: 0.054, costurada: 0.057 },
+    { tipo: "Vergê 85g", colada: 0.059, costurada: 0.061 },
+];
